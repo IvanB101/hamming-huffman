4853d3f57becbffc921b55e4930ee5b78d79731c
IvanB101 <ivanbrocas01@gmail.com>
2023-04-14 07:39:02 -0300

    algunas declaraciones

--- src/bitvec/bitvec.h
+++ src/bitvec/bitvec.h
@@ -1,6 +1,20 @@
 #ifndef BITVEC
 #define BITVEC
 
+typedef struct bitvec {
+    int* vector;
+    int primero;
+    int ultimo;
+};
 
+bitvec take(bitvec vector, int cantidad);
+
+bool put(bitvec vector_a, bitvec vector_de);
+
+bitvec and(bitvec a, bitvec b);
+
+bitvec or(bitvec a, bitvec b);
+
+bitvec xor(bitvec a, bitvec b);
 
 #endif 
