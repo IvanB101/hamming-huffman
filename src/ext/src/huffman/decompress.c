412525b064a5a3365197a40237d26f4fcc685bb8
luuloyola <lucianaloyola30@gmail.com>
2023-05-08 19:02:00 -0300

    gog

--- src/ext/src/huffman/decompress.c
+++ src/ext/src/huffman/decompress.c
@@ -8,4 +8,110 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *decompress(char *path, char *dest) { return "Not yet implemented"; }
+decoding_tree* init_node();
+
+decoding_tree* build_tree(encoding_tree tree);
+
+char *decompress(char *path, char *dest) {
+    FILE *fd, *res;
+    uint64_t info_bytes, file_size, buff_offset;
+    
+    fd = fopen(path, "rb");
+    if (!fd) {
+        return strerror(errno);
+    }
+    
+    res = fopen(dest, "wb");
+    if (!res) {
+        return strerror(errno);
+    }
+
+    encoding_tree tree;
+    fread((void *)&tree.distinct, sizeof(uint32_t), 1, fd);
+    for (int i = 0; i < tree.distinct; i++) {
+        char_info entry = tree.nodes[i];
+        // Character for the table entry
+        fread((void *)&entry.orig, 1, 1, fd);
+        // Length of character code
+        fread((void *)&entry.code_length, 1, 1, fd);
+        // New code for character
+        *entry.code = malloc(sizeof(entry.code_length));
+        fread((void *)&entry.code, entry.code_length, 1, fd);
+        // Fraction of original files characters equal to this one
+        fread((void *)&entry.prob, sizeof(double), 1, fd);
+    }
+
+    // Size in bytes of uncompressed information
+    fread((void *)&file_size, 1, sizeof(uint64_t), fd);
+    // Size in bytes of compressed information
+    fread((void *)&info_bytes, 1, sizeof(uint64_t), fd);
+    // Size in bites of compressed information
+    fread((void *)&buff_offset, 1, sizeof(uint64_t), fd);
+    
+    void *result = malloc(file_size);
+    void *buffer = malloc(file_size);
+    
+    // Compresed information
+    fread(buffer, 1, info_bytes, fd);
+
+    decoding_tree *root = build_tree(tree);
+
+    uint16_t buff_index = 0;
+    for (int i= 0; i < buff_offset; i++, buff_index++){
+        decoding_tree *aux = root;
+
+        char code = ((char *)buffer)[i];
+
+        while (aux->caract==NULL){
+            if (code){
+                aux = aux->der;
+            }
+            else{
+                aux = aux->izq;
+            }
+        }
+
+        move((void *)aux->caract, result, 0, buff_index, sizeof(char));
+    }
+
+    fwrite(result, 1, file_size, res);
+
+}
+
+decoding_tree* init_node(){
+    decoding_tree *node = (decoding_tree*)malloc(sizeof(decoding_tree));
+
+    node->caract = NULL;
+    node->der = NULL;
+    node->izq = NULL;
+
+    return node;
+}
+
+decoding_tree* build_tree(encoding_tree tree){
+    decoding_tree *root = init_node();
+
+    for (int i=0; i<tree.distinct; i++){
+        decoding_tree *aux = root;
+        char_info info = tree.nodes[i];
+        for(int j=0; j < info.code_length; j++){
+            if (info.code[j]){
+                if(aux->der == NULL){
+                    aux->der = init_node();
+                }
+                aux = aux->der;
+            }
+            else{
+                if(aux->izq == NULL){
+                    aux->izq = init_node();
+                }
+                aux = aux->izq;
+            }
+        }
+        
+        aux->caract = info.orig;
+    }
+
+    return root;
+}
+
--- src/ext/src/huffman/huffman.h
+++ src/ext/src/huffman/huffman.h
@@ -3,6 +3,13 @@
 
 #include <stdint.h>
 
+/*
+  @param orig original character
+  @param code is a pointer to the decoding code
+  @param code_length is the length of the code
+  @param prob is the probability of the character 
+
+*/
 typedef struct {
   char orig;
   char *code;
@@ -15,6 +22,17 @@ typedef struct {
     uint32_t distinct;
     char_info *nodes;
 } encoding_tree;
+
+/*
+ @param caract cotains the caracter that node represents
+ @param izq represents the code 0, also being the left son
+ @param der represents the code 1, also being the right son 
+*/
+typedef struct node {
+  char caract;
+  struct node *izq; // nodo izquierdo representa el 1
+  struct node *der; // nodo derecho representa el 0
+} decoding_tree;
 
 /**
  * Compresses a file a writes de result to another file
