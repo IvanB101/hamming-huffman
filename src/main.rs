0133d8436f01ad9cc35c6d81ea3e8dad476f5b17
IvanB101 <ivanbrocas01@gmail.com>
2023-05-31 15:51:50 -0300

    global callbacks

--- src/main.rs
+++ src/main.rs
@@ -8,59 +8,61 @@ use rfd::FileDialog;
 fn main() {
     let main_window = MainWindow::new().unwrap();
 
-    main_window.on_choose_file(move |operation| {
-        let valid_extentions: Vec<&str>;
+    main_window
+        .global::<State>()
+        .on_choose_file(move |operation| {
+            let valid_extentions: Vec<&str>;
 
-        match operation.as_str() {
-            "hamming" => {
-                valid_extentions = ["txt"].into();
+            match operation.as_str() {
+                "hamming" => {
+                    valid_extentions = ["txt"].into();
+                }
+                "dehamming" => {
+                    valid_extentions = ["HA1", "HA2", "HA3", "HE1", "HE2", "HE3"].into();
+                }
+                "corrupt" => {
+                    valid_extentions = ["HA1", "HA2", "HA3"].into();
+                }
+                "huffman" => {
+                    valid_extentions = ["txt", "doc", "docx"].into();
+                }
+                "dehuffman" => {
+                    valid_extentions = ["huf"].into();
+                }
+                _ => {
+                    valid_extentions = Vec::new();
+                }
             }
-            "dehamming" => {
-                valid_extentions = ["HA1", "HA2", "HA3", "HE1", "HE2", "HE3"].into();
-            }
-            "corrupt" => {
-                valid_extentions = ["HA1", "HA2", "HA3"].into();
-            }
-            "huffman" => {
-                valid_extentions = ["txt", "doc", "docx"].into();
-            }
-            "dehuffman" => {
-                valid_extentions = ["huf"].into();
-            }
-            _ => {
-                valid_extentions = Vec::new();
-            }
-        }
 
-        let path = match FileDialog::new()
-            .add_filter("", valid_extentions.as_ref())
-            .set_directory(".")
-            .pick_file()
-        {
-            Some(path) => path,
-            None => return,
-        };
+            let path = match FileDialog::new()
+                .add_filter("", valid_extentions.as_ref())
+                .set_directory(".")
+                .pick_file()
+            {
+                Some(path) => path,
+                None => return,
+            };
 
-        let file_name = path.as_path().to_str().unwrap().to_string();
+            let file_name = path.as_path().to_str().unwrap().to_string();
 
-        let error = match operation.as_str() {
-            "hamming" => ext::encode(file_name.to_owned(), 32),
-            "dehamming" => ext::decode(file_name.to_owned(), true),
-            "corrupt" => ext::corrupt(file_name.to_owned(), 0.50),
-            "huffman" => ext::compress(file_name.to_owned()),
-            "dehuffman" => ext::decompress(file_name.to_owned()),
-            _ => Err(FfiError {
-                message: "Invalid Operation".to_string(),
-            }),
-        };
+            let error = match operation.as_str() {
+                "hamming" => ext::encode(file_name.to_owned(), 32),
+                "dehamming" => ext::decode(file_name.to_owned(), true),
+                "corrupt" => ext::corrupt(file_name.to_owned(), 0.50),
+                "huffman" => ext::compress(file_name.to_owned()),
+                "dehuffman" => ext::decompress(file_name.to_owned()),
+                _ => Err(FfiError {
+                    message: "Invalid Operation".to_string(),
+                }),
+            };
 
-        match error {
-            Ok(_) => {}
-            Err(e) => {
-                println!("Error: {}", e);
+            match error {
+                Ok(_) => {}
+                Err(e) => {
+                    println!("Error: {}", e);
+                }
             }
-        }
-    });
+        });
 
     main_window.run().unwrap();
 }
