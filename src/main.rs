8c64f58028a78578835f48482ce523082eb9ae19
IvanB101 <ivanbrocas01@gmail.com>
2023-06-03 20:59:50 -0300

    mejora en operaciones y visualizador de archivo

--- src/main.rs
+++ src/main.rs
@@ -2,65 +2,131 @@ mod ext;
 
 slint::include_modules!();
 
-use ext::FfiError;
+use std::{fs::File, io::Read, rc::Rc};
+
+use ext::Extention;
 use rfd::FileDialog;
+use slint::{Model, SharedString};
 
 fn main() {
     let main_window = MainWindow::new().unwrap();
-    let main_window_weak = main_window.as_weak();
 
+    let default_errors: Vec<SharedString> = main_window.get_errors().iter().collect();
+    let errors = Rc::new(slint::VecModel::from(default_errors));
+    main_window.set_errors(errors.clone().into());
+
+    let default_in_progress: Vec<bool> = main_window.get_inProgress().iter().collect();
+    let in_progress = Rc::new(slint::VecModel::from(default_in_progress));
+    main_window.set_inProgress(in_progress.clone().into());
+
+    let default_file_text: Vec<SharedString> = main_window.get_file_text().iter().collect();
+    let file_text = Rc::new(slint::VecModel::from(default_file_text));
+    main_window.set_file_text(file_text.clone().into());
+
+    let errors_copy = errors.clone();
+    main_window.global::<State>().on_protect(move |value| {
+        let valid_extentions = ["txt"].into();
+
+        let path = match choose_file(valid_extentions) {
+            Some(val) => val,
+            None => return,
+        };
+
+        let block_size = value.parse().unwrap();
+
+        if let Err(e) = ext::encode(path, block_size) {
+            errors_copy.set_row_data(0, e.to_string().into());
+        }
+    });
+
+    let errors_copy = errors.clone();
+    main_window.global::<State>().on_desprotect(move |correct| {
+        let valid_extentions = ["HA1", "HA2", "HA3", "HE1", "HE2", "HE3"].into();
+
+        let path = match choose_file(valid_extentions) {
+            Some(val) => val,
+            None => return,
+        };
+
+        if let Err(e) = ext::decode(path, correct) {
+            errors_copy.set_row_data(1, e.to_string().into());
+        }
+    });
+
+    let errors_copy = errors.clone();
     main_window
         .global::<State>()
-        .on_operation(move |operation, value| {
-            let valid_extentions: Vec<&str>;
-
-            match operation.as_str() {
-                "hamming" => {
-                    valid_extentions = ["txt"].into();
-                }
-                "dehamming" => {
-                    valid_extentions = ["HA1", "HA2", "HA3", "HE1", "HE2", "HE3"].into();
-                }
-                "corrupt" => {
-                    valid_extentions = ["HA1", "HA2", "HA3"].into();
-                }
-                "huffman" => {
-                    valid_extentions = ["txt", "doc", "docx"].into();
-                }
-                "dehuffman" => {
-                    valid_extentions = ["huf"].into();
-                }
-                _ => {
-                    valid_extentions = Vec::new();
-                }
-            }
+        .on_corrupt(move |probability| {
+            let valid_extentions = ["HA1", "HA2", "HA3"].into();
 
             let path = match choose_file(valid_extentions) {
                 Some(val) => val,
                 None => return,
             };
 
-            if let Err(e) = match operation.as_str() {
-                "hamming" => {
-                    let block_size = value.parse().unwrap();
-
-                    ext::encode(path, block_size)
-                }
-                "dehamming" => {
-                    let correct = value.parse().unwrap();
-
-                    ext::decode(path, correct)
-                }
-                "corrupt" => ext::corrupt(path.to_owned(), 0.50),
-                "huffman" => ext::compress(path.to_owned()),
-                "dehuffman" => ext::decompress(path.to_owned()),
-                _ => Err(FfiError {
-                    message: "Invalid Operation".to_string(),
-                }),
-            } {
-                main_window_weak.unwrap().set_error(e.to_string().into());
+            if let Err(e) = ext::corrupt(path, probability.into()) {
+                errors_copy.set_row_data(2, e.to_string().into());
             }
         });
+
+    let errors_copy = errors.clone();
+    main_window.global::<State>().on_compress(move || {
+        let valid_extentions = ["txt", "doc", "docx"].into();
+
+        let path = match choose_file(valid_extentions) {
+            Some(val) => val,
+            None => return,
+        };
+
+        if let Err(e) = ext::compress(path) {
+            errors_copy.set_row_data(3, e.to_string().into());
+        }
+    });
+
+    let errors_copy = errors.clone();
+    main_window.global::<State>().on_decompress(move || {
+        let valid_extentions = ["huf"].into();
+
+        let path = match choose_file(valid_extentions) {
+            Some(val) => val,
+            None => return,
+        };
+
+        if let Err(e) = ext::decompress(path) {
+            errors_copy.set_row_data(4, e.to_string().into());
+        }
+    });
+
+    main_window.global::<State>().on_choose_file(move || {
+        let valid_extentions = [
+            "txt", "doc", "docx", "DE1", "DE2", "DE3", "DC1", "DC2", "DC3",
+        ]
+        .into();
+        let error_extensions = ["HE1", "HE2", "HE3"];
+
+        let path = match choose_file(valid_extentions) {
+            Some(val) => val,
+            None => return,
+        };
+
+        if let Some(_index) = error_extensions.iter().position(|&x| path.has_extention(x)) {
+            // TODO Return Vec<String> with errors
+        } else {
+            let mut new_file_text: Vec<SharedString> = Vec::new();
+
+            let mut contents = String::new();
+
+            let mut file = File::open(path).unwrap();
+
+            file.read_to_string(&mut contents).unwrap();
+
+            new_file_text.push(contents.into());
+            new_file_text.push("".into());
+            new_file_text.push("".into());
+
+            file_text.set_vec(new_file_text);
+        }
+    });
 
     main_window.run().unwrap();
 }
